e3f6e7a5a7054c2d5345bcdd0327b5caeedf5d8e
Ramakanth Muthyala <ramakanthmuthyala008@gmail.com>
2024-04-10 02:50:36 -0700

    react-basics1

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  const greetings = "hello world";
+  return (
+    <div className="appp">
+      <p className="app">{greetings}</p>
+    </div>
+  );
+}
